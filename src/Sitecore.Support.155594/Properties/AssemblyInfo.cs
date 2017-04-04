3f7ff0d68c2454c7854518c0d0d75a6c562463ac
DK\obr <obr@sitecore.net>
2017-04-04 13:38:05 +0300

    First commit

---
+++ src/Sitecore.Support.155594/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.155594")]
+[assembly: AssemblyProduct("Sitecore.Support.155594")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.155594/ResetStandardValue.cs
@@ -0,0 +1,62 @@
+﻿using Sitecore.Collections;
+using Sitecore.Data;
+using Sitecore.Data.Items;
+using Sitecore.Publishing.Pipelines.PublishItem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support
+{
+    public class ResetStandardValue
+    {
+        public virtual void Process(PublishItemContext context)
+        {
+            Item sourceItem = context.PublishHelper.GetSourceItem(context.ItemId);
+            Item targetItem = context.PublishHelper.GetTargetItem(context.ItemId);
+            if (sourceItem.SourceUri != null)
+            {
+                Item item = Database.GetItem(sourceItem.SourceUri);
+                if (item != null)
+                {
+                    FieldCollection fields = item.Fields;
+                    fields.ReadAll();
+                    foreach (Sitecore.Data.Fields.Field field in fields)
+                    {
+                        if (field.Key == "__renderings" && field.ContainsStandardValue)
+                        {
+                            if (sourceItem.Fields["__renderings"].Value == item.Fields["__renderings"].GetStandardValue())
+                            {
+                                using (new Sitecore.SecurityModel.SecurityDisabler())
+                                {
+                                    using (new Sitecore.Data.Items.EditContext(targetItem))
+                                    {
+                                        targetItem.Editing.BeginEdit();
+                                        targetItem.Fields["__renderings"].Reset();
+                                        targetItem.Editing.EndEdit();
+                                    }
+                                }
+                            }
+                        }
+                        if (field.Key == "__final renderings" && field.ContainsStandardValue)
+                        {
+                            if (sourceItem.Fields["__final renderings"].Value == item.Fields["__final renderings"].GetStandardValue())
+                            {
+                                using (new Sitecore.SecurityModel.SecurityDisabler())
+                                {
+                                    using (new Sitecore.Data.Items.EditContext(targetItem))
+                                    {
+                                        targetItem.Editing.BeginEdit();
+                                        targetItem.Fields["__final renderings"].Reset();
+                                        targetItem.Editing.EndEdit();
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
